9eb27f2997ee477e687e5fd450dd2f2ccd4651a7
Tuanne2108 <107233041+Tuanne2108@users.noreply.github.com>
2023-11-07 16:11:36 +0700

    Create routes and modify index

--- src/index.js
+++ src/index.js
@@ -4,7 +4,13 @@ const path = require("path");
 const hbs = require("express-handlebars");
 const morgan = require("morgan");
 const port = 3000;
+const route = require("./routes")
 
+//Body-parser
+app.use(express.json());
+app.use(express.urlencoded({
+  extended: true,
+}));
 //Static files
 app.use(express.static(path.join(__dirname, "public")));
 //Http logger
@@ -18,13 +24,8 @@ app.engine(
 );
 app.set("view engine", "hbs");
 app.set("views", path.join(__dirname, "resources\\views"));
-//Main
-app.get("/", (req, res) => {
-  res.render("home");
-});
-app.get("/news", (req, res) => {
-  res.render("news");
-});
+//Route init
+route(app);
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
