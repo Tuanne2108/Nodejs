fb1b36dd060259cbcd89d45f810ecbb8043d5f01
Tuanne2108 <107233041+Tuanne2108@users.noreply.github.com>
2023-11-26 13:51:35 +0700

    Prepare to create checkbox for restore and delete permanently

--- src/routes/me.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const meController = require("../controllers/MeController");
-
-router.get("/stored/products", meController.storedProducts);
-router.get("/trash/products", meController.deletedProducts)
-
-
-module.exports = router;
